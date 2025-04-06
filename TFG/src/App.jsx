ce406867c0a228d43422ac1d9117217c486256b0
KarenSequera <92042161+KarenSequera@users.noreply.github.com>
2025-04-06 13:58:35 +0200

    App.jsx comments

--- TFG/src/App.jsx
+++ TFG/src/App.jsx
@@ -1,16 +1,45 @@
 import { useState } from 'react'
 import { parseHoneypots, parseQuestions } from './utils/dataParser';
 import Questionnaire from './components/questionnaire';
-
+/**
+ * The main application component for the HoneyForm questionnaire.
+ */
 function App() {
-  const [stage, setStage] = useState('intro'); // The initial Stage is always the intro
-  const [sector, setSector] = useState(null); // Stores the selected sector
+/**
+   * State Variables:
+   * @property {string} stage - Tracks the current stage of the application. Possible values:
+   *   - 'intro': The intro screen.
+   *   - 'sectorSelection': Screen to select the sector.
+   *   - 'transportSelection': Screen to select the transport subsector.
+   *   - 'questionnaire': The questionnaire screen.
+   *   - 'recommendations': The recommendations screen.
+   * 
+   * @property {string} sector - Stores the selected sector or subsector. 
+   *   - values: 'healthcare', 'energy', 'financial', 'transport', 'air', 'road', 'maritime', 'railway'.
+   */
+  const [stage, setStage] = useState('intro'); // The initial Stage is always 'intro'.
+  const [sector, setSector] = useState(null);
 
+  /**
+   *************HANDLERS******************
+   */
+
+  /**
+   * Handles the transition from 'intro' stage to 'sectorSelection'
+   * @returns 
+   */
   const handleIntro = () => setStage('sectorSelection');
+
+  /**
+   * Handles the selection of a sector or subsector.
+   * @param {string} selectedSector 
+   */
   const handleSectorSelected = (selectedSector) => {
+    // If the selected sector is transport, the stage is set to  'transportSelection'
     if(selectedSector === 'transport'){
       setStage('transportSelection')
     }
+    // Any other value sets the stage to 'questionnaire' and sets the sector state variable accordingly.
     else{
       setSector(selectedSector);
       setStage('questionnaire');
@@ -31,6 +60,12 @@ function App() {
         height: "100vh" // Full viewport height
       }}
     >
+
+    {
+      /**
+       *************STAGES MANAGEMENT ******************
+      //  */
+    }
 
     {stage === 'intro' && (
       <div>
--- TFG/src/components/questionnaire.jsx
+++ TFG/src/components/questionnaire.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { parseHoneypots, parseQuestions } from '../utils/dataParser';
 
 const Questionnaire = ( { sector }) => {
 
